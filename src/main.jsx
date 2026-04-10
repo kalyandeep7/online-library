8176292d564fc45bdfb6b1cca1a9443f5a452fcd
kalyandeep7 <kalyandeep2001@gmail.com>
2026-04-10 10:24:42 +0530

    Configure React Router with all routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// Entry point: wraps the app with Redux store and React Router
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { store } from './store/store'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Provide Redux store to the entire app */}
+    <Provider store={store}>
+      {/* BrowserRouter enables React Router routing */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 )
